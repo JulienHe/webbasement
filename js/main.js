e8adf7c85731ae393109a786734b6a557baa4384
Julien He <hoshi.graphi@gmail.com>
2014-03-30 21:42:05 +0200

    V1

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//Basic variable
+var mathem, dataHeight,opacityElement, position;
+
+//Responsive navigation
+// var navigation = responsiveNav(".nav", {
+// 	customToggle: ".nav-toggle"
+// });
+
+//Scroll item 
+$(window).scroll(function(){
+	mathem = $(this).scrollTop(),
+	dataHeight = $('[data-background]').outerHeight() / 100;
+	opacityElement = mathem / dataHeight,
+	position = -mathem / 2;
+
+	$('[data-background]').css({'background-position' : "0 " + position +"px"});
+	opacityElement = parseFloat(1 - (opacityElement / 100));
+	$('[data-context]').css({
+		'padding-top' : -position,
+		'opacity' : opacityElement
+	});
+});
+
+//Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-21761886-1', 'webbasement.be');
+ga('send', 'pageview');
